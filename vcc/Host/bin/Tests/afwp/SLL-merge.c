f942f461560843c1cae73d3d7a74dace67c04b8d
Daejun Park <daejunpark@gmail.com>
2016-02-18 23:06:25 -0600

    fix SLL-merge

--- vcc/Host/bin/Tests/afwp/SLL-merge.c
+++ vcc/Host/bin/Tests/afwp/SLL-merge.c
@@ -14,22 +14,32 @@ Node * SLL_merge(Node * i, Node * j)
   _(assume mutable_list(j))
   Node * h = i;
   Node * k = NULL;
+
+      k = i;
+      i = i->next;
+      _(assume mutable_list(i))
+
   while(i != NULL) 
+    _(invariant srtl_lseg(h, k))
+    _(invariant srtl_lseg(k, i))
     _(invariant srtl(i))
     _(invariant srtl(j))
+
+    _(invariant k->next == i)
+
+    _(invariant \oset_disjoint(srtl_lseg_reach(h, k), \oset_singleton(k)))
+    _(invariant \oset_disjoint(srtl_lseg_reach(h, k), srtl_reach(i)))
+    _(invariant \oset_disjoint(srtl_lseg_reach(h, k), srtl_reach(j)))
+    _(invariant \oset_disjoint(\oset_singleton(k), srtl_reach(i)))
+    _(invariant \oset_disjoint(\oset_singleton(k), srtl_reach(j)))
     _(invariant \oset_disjoint(srtl_reach(i), srtl_reach(j)))
-    _(invariant srtl(h))
-    _(invariant srtl(k))
-    _(invariant srtl_lseg(h, k))
-    _(invariant k == NULL ==> h == i)
-    _(invariant k != NULL ==> \oset_disjoint(srtl_lseg_reach(h, k), srtl_reach(k)))
-    _(invariant k != NULL ==> \oset_disjoint(srtl_lseg_reach(h, k), srtl_reach(i)))
-    _(invariant k != NULL ==> \oset_disjoint(srtl_lseg_reach(h, k), srtl_reach(j)))
-    _(invariant k != NULL ==> !(\oset_in(k, srtl_reach(i))))
-    _(invariant k != NULL ==> !(\oset_in(k, srtl_reach(j))))
-    _(invariant k != NULL ==> k->next == i)
-    _(invariant srtl_lseg(k, i))
-    _(invariant \oset_disjoint(srtl_lseg_reach(k, i), srtl_reach(i)))
+
+    _(invariant h != k ==> sll_lseg_max_key(h, k) <= k->key)
+    _(invariant i != NULL ==> k->key <= sll_min_key(i))
+    _(invariant j != NULL ==> k->key <= sll_min_key(j))
+
+    _(invariant k != NULL)
+
     _(invariant mutable_list(i))
     _(invariant mutable_list(j))
   {
@@ -41,7 +51,7 @@ Node * SLL_merge(Node * i, Node * j)
       k->next = j;
       k = j;
       j = j->next;
-      _(assume mutable_list(i))
+      _(assume mutable_list(j))
       k->next = i;
     }
   }
