20bdb8563222847e62a97c5bf4f5f99423a65bb0
Daejun Park <daejunpark@gmail.com>
2016-02-18 23:11:43 -0600

    separate run_houdini.bat

---
+++ vcc/Host/bin/Tests/run_houdini.array.bat
@@ -0,0 +1,1 @@
+C:\boogie\Boogie\Boogie.exe /typeEncoding:m /prover:SMTLib /useArrayTheory /trace /noinfer /contractInfer Z:\InvariantSynthesis\repo\experiments\Vcc3Prelude.array.bpl %*
--- vcc/Host/bin/Tests/run_houdini.bat
+++
@@ -1,1 +0,0 @@
-C:\boogie\Boogie\Boogie.exe /typeEncoding:m /prover:SMTLib /useArrayTheory /trace /noinfer /contractInfer Z:\InvariantSynthesis\repo\vcdryad_bpl\Vcc3Prelude.new.bpl %*
